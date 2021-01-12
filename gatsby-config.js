8e6361022433064404107608af486dd6b264daa3
Aral <aral.tasher@rho.ai>
2021-01-11 22:16:12 -0500

    initial website setup

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Intro',
+    description:
+      'A site we built together during a full-day Frontend Masters Workshop',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+};
