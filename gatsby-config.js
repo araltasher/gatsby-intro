7fd86fd8db90ebbaaaade6fd4643fbbd7fd32927
Aral <aral.tasher@rho.ai>
2021-01-11 23:26:13 -0500

    set up blog posts with MDX

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,23 @@ module.exports = {
     description:
       'A site we built together during a full-day Frontend Masters Workshop',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout'),
+        },
+      },
+    },
+    {
+        resolve: 'gatsby-source-filesystem',
+        options: {
+            name: 'posts',
+            path: 'posts'
+        }
+    }
+  ],
 };
