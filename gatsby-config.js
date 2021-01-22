022ab50bfe2c2cc26b0ce7dd13c7223737a77e37
Aral <aral.tasher@rho.ai>
2021-01-22 10:41:04 -0500

    added Instagram integration
    added package analysis plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,20 @@ module.exports = {
         path: 'images',
       },
     },
+    {
+      resolve: 'gatsby-source-instagram',
+      options: {
+        username: '18588960',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-webpack-bundle-analyzer',
+      options: {
+        production: true,
+        disable: !process.env.ANALYZE_BUNDLE_SIZE,
+        generateStatsFile: true,
+        analyzerMode: 'static',
+      },
+    },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Layout from '../components/layout';
 import Hero from '../components/hero';
 import PostPreview from '../components/post-preview';
+import Insta from '../components/insta';
 import usePosts from '../hooks/use-posts';
 
 export default () => {
@@ -16,6 +17,7 @@ export default () => {
         {posts.map(post => (
           <PostPreview key={post.slug} post={post} />
         ))}
+        <Insta />
       </Layout>
     </>
   );
